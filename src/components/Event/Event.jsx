e98b2fabedb5660965ad7cd76f719bd05a8f70e5
Vitaliy2773 <vitalii64773@gmail.com>
2023-10-10 00:55:24 -1100

    add-Component

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,28 +1,38 @@
-import { FaMapMarkerAlt,FaUserAlt,FaCalendarAlt,FaClock } from "react-icons/fa";
+import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from "react-icons/fa";
+import { formatEventStart, formatEventDuration } from "helpers";
+import { iconSize } from "constants";
 import PropTypes from 'prop-types';
 import css from "./Event.module.css"
 
-export const Event = ({name,location,speaker,type,start,end}) => {
+
+export const Event = ({ name, location, speaker, type, start, end }) => {
+    
+
+    const formatStartTime = formatEventStart(start);
+    const duration = formatEventDuration(start, end);
+
+    console.log(css[type]);
+    console.log(css);
     return (
   <div className={css.event}>
             <h2 className={css.title}>{name}</h2>
   <p className={css.info}>
-                <FaMapMarkerAlt className={css.icon} size={16} />
+                <FaMapMarkerAlt className={css.icon} size={iconSize.sm} />
     {location}
   </p>
   <p className={css.info}>
-                <FaUserAlt className={css.icon} size={16} />
+                <FaUserAlt className={css.icon} size={iconSize.sm} />
     {speaker}
   </p>
   <p className={css.info}>
-    <FaCalendarAlt className={css.icon} size={16}/>
-    {start}
+    <FaCalendarAlt className={css.icon} size={iconSize.sm}/>
+    {formatStartTime}
   </p>
   <p className={css.info}>
-    <FaClock className={css.icon} size={16}/>
-    Duration
+    <FaClock className={css.icon} size={iconSize.sm}/>
+    {duration}
   </p>
-            {/* <span className="chip free|paid|vip">{type}</span> */}
+            <span className={`${css.chip} ${css[type]}`}>{type}</span>
 </div>
     )
 }
--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -25,9 +25,9 @@ EventBoard.propTypes = {
     location: PropTypes.string.isRequired,
     speaker: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    time: {
+    time: PropTypes.exact({
       start: PropTypes.string.isRequired,
       end: PropTypes.string.isRequired,
-    }
+    })
   },))
 }
